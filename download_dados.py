e4e58a4928160dede095c37edb4fa81202077287
Matheus Gonella <matgonella1@gmail.com>
2024-04-02 21:02:36 -0300

    Update download_dados.py

--- download_dados.py
+++ download_dados.py
@@ -4,6 +4,7 @@ import zipfile
 import datetime
 
 # creating 'dados_cvm' folder
+print('Creating dados_cvm folder...\n')
 folder = 'dados_cvm'
 os.makedirs(folder)
 os.chdir(os.getcwd() + f'\\{folder}')
@@ -12,7 +13,12 @@ ano_atual = datetime.datetime.now().year
 anos = range(2010, ano_atual)
 url = 'https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/DFP/DADOS/'
 
+print('Downloading data...\n')
+
 # downloading data
 for ano in anos:
     download = requests.get(url + f'dfp_cia_aberta_{ano}.zip')
     open(f'dfp_cia_aberta_{ano}.zip', 'wb').write(download.content)
+    print(f'Finished downloading dfp_cia_aberta_{ano}.zip')
+
+input('Finished! Press Enter to exit...')
